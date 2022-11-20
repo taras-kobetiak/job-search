41a1541502bce1064c6ffc9768a569a2135c0cd0
Taras <kobetiakt@gmail.com>
2022-11-20 13:25:29 +0200

    add css to job-list page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
+    screens: {
+      'xs': '576px',
+      ...defaultTheme.screens,
       '3xl': '1920px'
     },
   },
