1c901c42486c53ff8b036173e8236d92988b2eb7
Taras <kobetiakt@gmail.com>
2022-11-21 19:41:43 +0200

    to deploying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoadingService } from './services/loading/loading-service.service';
 
